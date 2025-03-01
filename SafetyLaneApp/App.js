e474888d2696e2ce6c05dc7baf0f51895d510c0e
gavinz1 <gavinzhang111@gmail.com>
2025-03-01 18:45:24 -0500

    proof of concept movement and turning

--- SafetyLaneApp/App.js
+++ SafetyLaneApp/App.js
@@ -60,6 +60,10 @@ export default function App() {
   const [coneSeperation, setConeSeperation] = useState(1);
   const [distances, setDistances] = useState([]); //repurposed for receiving data from esp
   const [headings, setHeadings] = useState([]); //repurposed for receiving data from esp
+  const [distances2, setDistances2] = useState([]); //repurposed for receiving data from esp
+  const [headings2, setHeadings2] = useState([]); //repurposed for receiving data from esp
+  const [leadHeading, setLeadHeading] = useState(0); //repurposed for receiving data from esp
+  const [turnsMade, setTurnsMade] = useState(0); //repurposed for receiving data from esp
   const [permissions, setPermissions] = useState(false);
   const [dotColor, setDotColor] = useState("transparent");
 
@@ -215,28 +219,28 @@ export default function App() {
 
     //TODO: the following code is horrific, maybe clean it up later
 
-    // if (deviceNames.includes("SafetyLane_2")) {
-    //   console.log("in dnames");
-    //   let d2 = devicesToConnect["SafetyLane_2"];
-    //   // console.log("d41=2: ", d4);
-    //   let services = await d2.services();
-    //   let service = services.find((service) => service.uuid === SERVICE_UUID_2);
-    //   let characteristics = await service.characteristics();
-    //   let stepDataCharacteristic = characteristics.find(
-    //     (char) => char.uuid === CHARACTERISTIC_UUID_2
-    //   );
-    //   console.log("finding step data char uuid of", CHARACTERISTIC_UUID_2)
-    //   setStepDataChar2(stepDataCharacteristic);
-    //   stepDataCharacteristic.monitor((error, char) => {//monitor is what lets us recieve information from the board
-    //     if (error) {
-    //       console.error(error);
-    //       return;
-    //     }
-    //     const rawData = atob(char.value);
-    //     console.log("Received board 2 data:", rawData);
-    //     // addData(rawData);
-    //   });
-    // };
+    if (deviceNames.includes("SafetyLane_2")) {
+      console.log("in dnames");
+      let d2 = devicesToConnect["SafetyLane_2"];
+      // console.log("d41=2: ", d4);
+      let services = await d2.services();
+      let service = services.find((service) => service.uuid === SERVICE_UUID_2);
+      let characteristics = await service.characteristics();
+      let stepDataCharacteristic = characteristics.find(
+        (char) => char.uuid === CHARACTERISTIC_UUID_2
+      );
+      console.log("finding step data char uuid of", CHARACTERISTIC_UUID_2)
+      setStepDataChar2(stepDataCharacteristic);
+      stepDataCharacteristic.monitor((error, char) => {//monitor is what lets us recieve information from the board
+        if (error) {
+          console.error(error);
+          return;
+        }
+        const rawData = atob(char.value);
+        console.log("Received board 2 data:", rawData);
+        addData2(rawData);
+      });
+    };
     if (deviceNames.includes("SafetyLane_3")) {
       console.log("in dnames");
       let d3 = devicesToConnect["SafetyLane_3"];
@@ -257,6 +261,11 @@ export default function App() {
         const rawData = atob(char.value);
         console.log("Received board 3 data:", rawData);
         // addData(rawData);
+        const values = rawData.split(",");
+        const turns = values[0];
+        const head_val = values[1]    
+        setTurnsMade(turns);
+        setLeadHeading(head_val);
       });
     };
     if (deviceNames.includes("SafetyLane_4")) {
@@ -278,7 +287,7 @@ export default function App() {
         }
         const rawData = atob(char.value);
         console.log("Received board 4 data:", rawData);
-        // addData(rawData);
+        addData(rawData);
       });
     };
 
@@ -431,9 +440,10 @@ export default function App() {
         console.log(single_robot_sending_device_3);
         // Find the specfied device and send data
         if (single_robot_sending_device_1 == 1){
-          deviceID = deviceId_newesp32_1;
-          serviceUUID = SERVICE_UUID_1;
-          characteristicUUID = CHARACTERISTIC_UUID_1;
+          //TODO:: CHANGE THIS BACK TO 1 after testing
+          deviceID = deviceId_newesp32_3;
+          serviceUUID = SERVICE_UUID_3;
+          characteristicUUID = CHARACTERISTIC_UUID_3;
 
           console.log("ServiceUUID is:", serviceUUID);
           console.log("CharacteristicUUID is: ", characteristicUUID);
@@ -449,8 +459,8 @@ export default function App() {
         }
         else if (single_robot_sending_device_2 == 1){
           deviceID = deviceId_newesp32_2;
-          serviceUUID = SERVICE_UUID_3; //changed from 2
-          characteristicUUID = CHARACTERISTIC_UUID_3; //changed from 2
+          serviceUUID = SERVICE_UUID_2; 
+          characteristicUUID = CHARACTERISTIC_UUID_2; 
 
           console.log("ServiceUUID is:", serviceUUID);
           console.log("CharacteristicUUID is: ", characteristicUUID);
@@ -516,6 +526,26 @@ export default function App() {
       return prev;
     })
   };
+  function addData2(val) {
+    // "given string "0.31,51.123" split into 2 
+    const values = val.split(",");
+    const dist_val = values[0];
+    const head_val = values[1]
+    setDistances2((prev) => {
+      console.log("running addDistance: ", prev, " vs ", dist_val)
+      if (prev.length < 5) {
+        return [...prev, dist_val];
+      }
+      return prev;
+    })
+    setHeadings2((prev) => {
+      console.log("running addDistance: ", prev, " vs ", head_val)
+      if (prev.length < 5) {
+        return [...prev, head_val];
+      }
+      return prev;
+    })
+  };
   async function handleRealign(value) {
     console.log("realign Pressed Down ", value);
     setDistances([]); //init to empty array
@@ -525,24 +555,96 @@ export default function App() {
       await sendData(value);
       await delay(1000); //delay in ms, change depending on in person trials
     }
-    // await delay(1000); //delay in ms, change depending on in person trials
-    // console.log("printing distances array", distances);
-
-    // const sortedArr = distances.sort((a,b) => a - b);
-    // console.log("printing distances =========");
-    // for (let i = 0; i < 5; i++ ) {
-    //   console.log("i=",i, "| d=", sortedArr[i]);
-    // }
-
   };  
+  async function handleTurnEnd() {
+    // -cone1 is moved forward a preset amount, say 3xseperation distance
+    //either use delay or do it inside arduino code
+    //CONE 1 START ====
+    await sendData(1);
+    await delay(100 * 3 * coneSeperation);
+    await sendData(0);
+
+
+    //CONE 2 START ====
+    // -code switches to send to cone 2 only
+    single_robot_sending_device_1 = 0
+    single_robot_sending_device_2 = 1
+    //first move cone 2 1 seperation distance forwards
+    await sendData(1);
+    await delay(100 * coneSeperation);
+    await sendData(0);
+ 
+    //turn robot 2 the required distance
+    let turns_copy = turnsMade;
+    if (turns_copy > 0) {
+      //>0 means right turn
+      for (let i = 0; i < turns_copy; i++) {
+        await sendData(3)
+        await delay(100); //either use this delay or a for loop inside the arduino code
+      }
+      await sendData(0)
+    } else if (turns_copy < 0) {
+      // <0 means left turn
+      turns_copy = turns_copy * -1;
+      for (let i = 0; i < turns_copy; i++) {
+        await sendData(2)
+        await delay(100); //either use this delay or a for loop inside the arduino code
+      }
+      await sendData(0)
+      turns_copy = turns_copy * -1;
+    }
+    else {
+      //0 degree turn, just move forward
+    }    
+    // cone 2 moves forward 2xseperation distance
+    await sendData(1);
+    await delay(100 * 2 * coneSeperation);
+    await sendData(0);
+
+    //CONE 3 START ====
+    // single_robot_sending_device_2 = 0
+    // single_robot_sending_device_3 = 1
+
+    //cone 3 moves forwards 2 cone seperations 
+    // await sendData(1);
+    // await delay(100 * 2 * coneSeperation);
+    // await sendData(0);
+
+    // if (turns_copy > 0) {
+    //   //>0 means right turn
+    //   for (let i = 0; i < turns_copy; i++) {
+    //     await sendData(3)
+    //     await delay(100); //either use this delay or a for loop inside the arduino code
+    //   }
+    //   await sendData(0)
+    // } else if (turns_copy < 0) {
+    //   // <0 means left turn
+    //   turns_copy = turns_copy * -1;
+    //   for (let i = 0; i < turns_copy; i++) {
+    //     await sendData(2)
+    //     await delay(100); //either use this delay or a for loop inside the arduino code
+    //   }
+    //   await sendData(0)
+    //   turns_copy = turns_copy * -1;
+    // }
+    // else {
+    //   //0 degree turn, just move forward
+    // }    
+    // await sendData(1);
+    // await delay(100 * coneSeperation);
+    // await sendData(0);
+
+    // sendData(0);
+  };
   function handleRelease() {
     console.log("button released", 0);
     sendData(0);
   };
   function onSubmitSeperation() {
     console.log("new seperation: ", coneSeperation)
-    newNum = +coneSeperation + 50
-    sendData(newNum);
+    
+    // newNum = +coneSeperation + 50
+    sendData(coneSeperation);
   }
 
   useEffect(() => {
@@ -566,9 +668,8 @@ export default function App() {
     }}, [permissions]);
 
     useEffect(()=>{
-      console.log("inside useEffect")
-      //check that we have distance + heading
-      if (distances.length >= 5) { //add condiiton for compass check
+      //this check is for the middle robot in a line of three
+      if (distances.length >= 5 && headings.length >= 5) { //add condiiton for compass check
         //calculation
 
         const sortedDist = distances.sort((a,b) => a - b);
@@ -581,201 +682,32 @@ export default function App() {
         console.log("selected median as ", sortedHead[2])
         //how are we gonna error correct?
         //hard code a couple groups, ex: within 30deg of leader, within 90, within 180, then run a hardcoded case 
-    
+        //if distance is <= 4.5, move robot backwards 
+        //if distance is >= 5.5, move robot forwards
+        //if heading is 30deg or more to the left of leading robot, move it backwards a certain amount, then correct 
+        //if heading is 30deg or more to the left of leading robot, move it backwards a certain amount, then correct 
 
       } 
       //send data to each robot 
     }, [distances])
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.connectionStatus}>{connectionStatus}</Text>
-//       <View style={styles.rightAligned}>
-//         <Pressable style={styles.realignButton} onPressIn={() => handlePress(9)}>
-//           <Text style={styles.buttonText}>Send To All Devices</Text>
-//           <View style={[styles.dot, { backgroundColor: dotColor }]} />
-//         </Pressable>
-//       </View>
-//       <View style={styles.topRight}>
-//         <View style={styles.inputContainer}>
-//           <Text style={styles.buttonText}>Cone Separation: </Text>
-//           <TextInput
-//             style={styles.textInput}
-//             onChangeText={setConeSeperation}
-//             onSubmitEditing={onSubmitSeperation}
-//             value={coneSeperation.toString()}
-//             defaultValue="1"
-//             keyboardType="numeric"
-//           />
-//           <Text style={styles.buttonText}>m</Text>
-//         </View>
-//       </View>
-//       <View style={styles.centered}>
-//         <Pressable style={styles.realignButton} onPressIn={() => handlePress(6)}>
-//           <Text style={styles.buttonText}>Realign Cones</Text>
-//           <View style={[styles.dot, { backgroundColor: dotColor }]} />
-//         </Pressable>
-//         <View style={styles.dPadContainer}>
-//           <Pressable
-//             style={[styles.buttonContainer, styles.upButton]}
-//             onPressIn={() => handlePress(1)}
-//             onPressOut={handleRelease}
-//           >
-//             <Text style={styles.buttonText}>Forward</Text>
-//           </Pressable>
-//           <View style={styles.middleRow}>
-//             <Pressable
-//               style={[styles.buttonContainer, styles.leftButton]}
-//               onPressIn={() => handlePress(2)}
-//               onPressOut={handleRelease}
-//             >
-//               <Text style={styles.buttonText}>Left</Text>
-//             </Pressable>
-//             <Pressable
-//               style={[styles.buttonContainer, styles.rightButton]}
-//               onPressIn={() => handlePress(3)}
-//               onPressOut={handleRelease}
-//             >
-//               <Text style={styles.buttonText}>Right</Text>
-//             </Pressable>
-//           </View>
-//           <Pressable
-//             style={[styles.buttonContainer, styles.downButton]}
-//             onPressIn={() => handlePress(4)}
-//             onPressOut={handleRelease}
-//           >
-//             <Text style={styles.buttonText}>Back</Text>
-//           </Pressable>
-//         </View>
-//       </View>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
+    useEffect(()=>{
+      //check that we have distance + heading
+      if (distances2.length >= 5 && headings2.length >=5) { //add condiiton for compass check
+        //calculation
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#1c1c1e',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     padding: 20,
-//   },
-//   connectionStatus: {
-//     color: '#00ffff',
-//     fontSize: 20,
-//     fontFamily: 'Courier', 
-//     marginBottom: 20,
-//     textAlign: 'center',
-//     backgroundColor: '#333',
-//     padding: 10,
-//     borderRadius: 5,
-//     width: '90%',
-//   },
-//   topRight: {
-//     position: 'absolute',
-//     top: 50,
-//     right: 20,
-//   },
-//   inputContainer: {
-//     flexDirection: 'row',
-//     alignItems: 'center',
-//     backgroundColor: '#444',
-//     borderRadius: 5,
-//     padding: 10,
-//   },
-//   textInput: {
-//     color: '#00ffff',
-//     backgroundColor: '#222',
-//     padding: 5,
-//     borderRadius: 3,
-//     marginHorizontal: 5,
-//     textAlign: 'center',
-//     width: 40,
-//   },
-//   centered: {
-//     alignItems: 'center',
-//   },
-//   realignButton: {
-//     alignItems: 'center',
-//     backgroundColor: '#ff4500',
-//     borderRadius: 5,
-//     marginBottom: 20,
-//     padding: 10,
-//     width: 200,
-//     flexDirection: 'row',
-//     justifyContent: 'center',
-//     position: 'relative',
-//   },
-//   dPadContainer: {
-//     width: 250,
-//     height: 250,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#333',
-//     borderRadius: 125,
-//     padding: 10,
-//   },
-//   middleRow: {
-//     flexDirection: 'row',
-//     justifyContent: 'space-between',
-//     width: '100%',
-//     marginVertical: 5,
-//   },
-//   buttonContainer: {
-//     alignItems: 'center',
-//     backgroundColor: '#ff4500',
-//     borderRadius: 15,
-//     padding: 15,
-//     margin: 5,
-//     width: 85,
-//     height: 85,
-//     justifyContent: 'center',
-//   },
-//   upButton: {
-//     position: 'absolute',
-//     top: 0,
-//     left: '50%',
-//     marginLeft: -35,
-//   },
-//   downButton: {
-//     position: 'absolute',
-//     bottom: 0,
-//     left: '50%',
-//     marginLeft: -35,
-//   },
-//   leftButton: {
-//     position: 'absolute',
-//     left: 0,
-//     top: '50%',
-//     marginTop: -40,
-//   },
-//   rightButton: {
-//     position: 'absolute',
-//     right: 0,
-//     top: '50%',
-//     marginTop: -40,
-//   },
-//   buttonText: {
-//     color: 'white',
-//     fontWeight: 'bold',
-//     fontSize: 14,
-//     textAlign: 'center',
-//   },
-//   dot: {
-//     width: 10,
-//     height: 10,
-//     borderRadius: 5,
-//     position: 'absolute',
-//     right: 10,
-//     top: 10,
-//   },
-//   rightAligned: {
-//     position: 'absolute',
-//     right: 20,
-//     top: '50%',
-//     transform: [{ translateY: -20 }], // Center vertically
-//   },
-// });
+        const sortedDist = distances2.sort((a,b) => a - b);
+        const sortedHead = headings2.sort((a,b) => a - b);
+        console.log("printing distances =========");
+        for (let i = 0; i < 5; i++ ) {
+          console.log("i=",i, "| d=", sortedDist[i]);
+        }
+        console.log("selected median as ", sortedDist[2])
+        console.log("selected median as ", sortedHead[2])
+        //how are we gonna error correct?
+        //hard code a couple groups, ex: within 30deg of leader, within 90, within 180, then run a hardcoded case 
+      } 
+      //send data to each robot 
+    }, [distances2])
 
 return (
   <View style={styles.container}>
@@ -829,8 +761,24 @@ return (
 
     {/* Realign Cones Button */}
     <View style={styles.centered}>
-      <Pressable style={styles.realignButton} onPressIn={() => handleRealign(6)}>
+    <Pressable style={styles.realignButton} onPressIn={() => handleRealign(6)}>
         <Text style={styles.buttonText}>Realign Cones</Text>
+        <View style={[styles.dot, { backgroundColor: dotColor }]} />
+      </Pressable>
+      <Pressable style={styles.realignButton} onPressIn={() => {
+        //disable other button inputs
+        single_robot_sending_device_1 = 1
+        all_devices = 0;
+        handlePress(7)
+        }}>
+        <Text style={styles.buttonText}>Start Turn</Text>
+        <View style={[styles.dot, { backgroundColor: dotColor }]} />
+      </Pressable>
+      <Pressable style={styles.realignButton} onPressIn={() => {
+        //re-enable other button inputs
+        handleTurnEnd()
+        }}>
+        <Text style={styles.buttonText}>End Turn</Text>
         <View style={[styles.dot, { backgroundColor: dotColor }]} />
       </Pressable>
 
